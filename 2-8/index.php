0c87922ce05cf0212cd01ff223879caf1b4e26b4
yuya-yuya <yytyfp5115@icloud.com>
2020-12-02 20:17:01 +0900

    foreach
    foreach

---
+++ 2-8/index.php
@@ -0,0 +1,24 @@
+<?php
+// $fruits = ["りんご", "みかん", "もも"];
+
+// foreach ($fruits as $value) {
+//   echo $value;
+// }
+
+// echo '<br>';
+
+// $fruits = ["apple" => "りんご" , "orange" => "みかん" , "peach" => "もも"];
+
+// foreach($fruits as $key => $value){
+//   echo $key;
+//   echo $value;
+
+$fruits = ["apple" => "りんご" , "orange" => "みかん" , "peach" => "もも"];
+
+foreach($fruits as $key => $value){
+  echo $key ; 
+  echo "といったら" ;
+  echo $value;
+  echo '<br>';
+}
+?>
