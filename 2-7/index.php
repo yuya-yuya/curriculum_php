4a9be849e2c44144ad88c258e82f25a025f13912
yuya-yuya <yytyfp5115@icloud.com>
2020-12-02 19:48:31 +0900

    配列、連想配列
    配列、連想配列

---
+++ 2-7/index.php
@@ -0,0 +1,30 @@
+<?php
+// $countries = ["america","japan","china","korea"];
+// echo $countries[0];
+// echo $countries[0];
+// echo $countries[1];
+// echo $countries[3];
+// var_dump($countries);
+
+// echo '<br>';
+// $fruits = ["apple" => "りんご" , "orange" => "みかん" , "grape" => "ぶどう"];
+// echo $fruits["apple"];
+// echo $fruits["orange"];
+// echo $fruits["grape"];
+// var_dump($fruits);
+
+// echo '<br>';
+// $fruits = ["りんご", "みかん", "ぶどう"];
+// $fruits[3] = "もも";
+// var_dump($fruits);
+
+// echo '<br>';
+// $fruits = ["apple" => "りんご", "orange" => "みかん", "grape" => "ぶどう"]; 
+// $fruits["peach"] = "もも";
+
+$color = ["red" => "赤" , "blue" => "青", "green" => "緑"];
+var_dump($color);
+echo '<br>';
+$color["yellow"] = "黄色";
+var_dump($color);
+?>
