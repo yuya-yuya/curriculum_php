d0cc80cc04c75e9d942a2adec4308aeaa07e597e
yuya-yuya <yytyfp5115@icloud.com>
2020-12-04 16:07:53 +0900

    additional commit
    additional commit

---
+++ 2-11/index.php
@@ -0,0 +1,10 @@
+<form action="result.php" method="get">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="email" name="e_mail" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+ 
---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_GET['my_name'];
+$password = $_GET['password'];
+$mail = $_GET[e_mail];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは,<?php echo $mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
